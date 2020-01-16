94618b499ce75ab72fb1e87ff551e8ed9eaf8112
karenjli <karenjy.li@gmail.com>
2020-01-16 17:54:29 -0600

    Day 2 of Intro to Python II Project

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,37 @@
 from room import Room
 from player import Player
+from item import Item
+
+#  Declare all items
+items = {
+    "coins": Item("coins",
+                  "Some coins that you can buy more stuff"),
+    "sword": Item("sword",
+                  "It does what a sword does."),
+    "hammer": Item("hammer", "It chops things."),
+    "gun": Item("gun", "It shoots."),
+    "arrow": Item("arrow", "With a bow, it does everything")
+}
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [items["coins"]]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [items["sword"]]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [items["hammer"]]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [items["gun"]]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [items["arrow"]]),
 }
 
 
@@ -39,39 +51,58 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player_name=input("Enter character name: ")
+player_name = input("Enter character name: ")
 player = Player(player_name, room["outside"])
 # Write a loop that:
 
 while True:
-# * Prints the current room name
-   
-# * Prints the current description (the textwrap module might be useful here).
+    # * Prints the current room name
+
+    # * Prints the current description (the textwrap module might be useful here).
     # print(room[player.current_room])
-    print(f"{player.room}, {player.room.description}")
+    print(f"{player.room}, Description:{player.room.description}, Items in room:{player.room.items}")
+    print(f"Your Inventory: {player.inventory}")
+    # print all the items
+    # for item in {player.room}:
+    #     print(f"Item:{item.name}, Description:{item.description}")
+
 # * Waits for user input and decides what to do.
     player_input = input("Enter your next move: ")
+    print(len(player_input))
 # If the user enters a cardinal direction, attempt to move to the room there.
-    if len(player_input)==1:
+
+    if len(player_input) == 1:
         try:
-            if player_input =="n":
-                player.room=player.room.n_to
-            elif player_input=="s":
-                player.room=player.room.s_to
-            elif player_input=="e":
-                player.room=player.room.e_to
-            elif player_input=="w":
-                player.room=player.room.w_to 
-            elif player_input=="q":
-                print("You have left the game. Thank you for playing.")  
+            if player_input == "n":
+                player.room = player.room.n_to
+            elif player_input == "s":
+                player.room = player.room.s_to
+            elif player_input == "e":
+                player.room = player.room.e_to
+            elif player_input == "w":
+                player.room = player.room.w_to
+            elif player_input == "q":
+                print("You have left the game. Thank you for playing.")
                 break
-         
         except:
             print("Wrong move! Try again.")
+
+    if len(player_input) >= 2:
+        print(f'get {item.name}')
+        try:
+            if player_input == f"get {item.name}":
+                player.add_item(item)
+                print(f"{item.name} added to your inventory")
+            elif player_input.contain("drop"):
+                player.drop_item(item)
+                print(f"{player_input[1]} removed from your inventory")
+        except:
+            print("Invalid Action! Try again.")
+
 
 else:
     print("Who's the player?")
 # Print an error message if the movement isn't allowed.
-    
+
 
 # If the user enters "q", quit the game.
--- src/player.py
+++ src/player.py
@@ -1,12 +1,36 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
 class Player:
     def __init__(self, name, room):
-        self.name=name
-        self.room=room
-    
+        self.name = name
+        self.room = room
+        self.inventory = []
+
     def __str__(self):
         return f"Player:{self.name}"
 
-    def __repr__(self):
-        return f"Player({repr(self.name, self.room)})"
+    # def __repr__(self):
+    #     return f"Player({repr(self.name, self.room)})"
+
+    def add_item(self, item):
+        if item in self.room.items:
+            self.room.items.remove(item)
+            self.inventory.append(item)
+            print(f"Added {item.name} to inventory.")
+        else:
+            print("You got nothing.")
+
+    def drop_item(self, item):
+        if item in self.items:
+            self.inventory.remove(item)
+            self.room.items.append(item)
+            print(f"{item.name} removed from your inventory.")
+
+    def view_inventory(self):
+        if len(self.inventory) > 0:
+            for i in self.inventory:
+                print(i.name)
+        else:
+            print("There is nothing in your inventory now")
